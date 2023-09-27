b2600dd48433f427450d6b9cd2125e39281775ed
Sebastian3xx7 <smatei345@gmail.com>
2023-09-27 11:31:48 +0300

    Initial commit

---
+++ src/test/java/com/example/myplannerApp/MyplannerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myplannerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyplannerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
